1d562cc4a7d2b6d85ed0dbd819b6311a0ddf5bf0
ChangYong-Oh <changyong.oh0224@gmail.com>
2017-11-20 23:14:09 +0100

    multiple runs

---
+++ multiple_HPOlib_run.py
@@ -0,0 +1,25 @@
+import time
+import sys
+import os.path
+import subprocess
+
+
+def multiple_runs(optimizer_dir, working_dir):
+	n_sample_run = 5
+	process_list = []
+	for i in range(n_sample_run):
+		cmd_str = 'HPOlib-run -o ' + optimizer_dir + ' --cwd ' + working_dir
+		f = open(os.devnull, 'w')
+		process = subprocess.Popen(cmd_str, shell=True, stdout=f, stderr=f)
+		process_list.append(process)
+	while n_running > 0:
+		time.sleep(60)
+		running = [elm.poll() is None for elm in process_list]
+		n_running = running.count(True)
+		print('%d/%d is still running %s' % (n_running, n_sample_run, time.strftime("%H:%M:%S")))
+		sys.stdout.flush()
+	print('Done')
+
+
+if __name__ == '__main__':
+	multiple_runs(sys.argv[1], sys.argv[2])
