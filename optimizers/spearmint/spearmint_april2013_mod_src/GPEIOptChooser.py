dac6198fdbf8a6d710028b6a2588e05b7cd5f1ac
ChangYong-Oh <changyong.oh0224@gmail.com>
2017-11-24 19:51:41 +0100

    up to date

--- optimizers/spearmint/spearmint_april2013_mod_src/GPEIOptChooser.py
+++ optimizers/spearmint/spearmint_april2013_mod_src/GPEIOptChooser.py
@@ -31,7 +31,7 @@ import scipy.linalg   as spla
 import scipy.stats    as sps
 import scipy.optimize as spo
 import cPickle
-from pathos import multiprocessing
+import multiprocessing
 
 from Locker import *
 
@@ -234,32 +234,32 @@ class GPEIOptChooser:
             # cand = np.vstack((cand, cand2))
 
             # Optimize each point in parallel
-            # pool = multiprocessing.Pool(self.grid_subset)
-            # results = [pool.apply_async(optimize_pt,args=(
-            #             c,b,comp,pend,vals,copy.copy(self))) for c in cand2]
-
-            n_cpu = float(multiprocessing.cpu_count())
             pool = multiprocessing.Pool(self.grid_subset)
-            results = []
-            process_started = [False] * self.grid_subset
-            process_running = [False] * self.grid_subset
-            process_index = 0
-            start_time = time.time()
-            while process_started.count(False) > 0:
-                time.sleep(1)
-                elapsed_time = time.time() - start_time
-                n_running = float(max(1, process_running.count(True)))
-                loadavg_1min = os.getloadavg()[0]
-                loadavg_discount = 16.0 if elapsed_time < 60 else 0
-                run_more = (loadavg_1min - loadavg_discount) / n_running < n_cpu - loadavg_1min
-                if run_more:
-                    results.append(pool.apply_async(optimize_pt, args=(cand2[process_index],b,comp,pend,vals,copy.copy(self))))
-                    process_started[process_index] = True
-                    process_running[process_index] = True
-                    process_index += 1
-            while process_running.count(True) > 0:
-                time.sleep(1)
-                process_running = [not p.ready() for p in results]
+            results = [pool.apply_async(optimize_pt,args=(
+                        c,b,comp,pend,vals,copy.copy(self))) for c in cand2]
+
+            # n_cpu = float(multiprocessing.cpu_count())
+            # pool = multiprocessing.Pool(self.grid_subset)
+            # results = []
+            # process_started = [False] * self.grid_subset
+            # process_running = [False] * self.grid_subset
+            # process_index = 0
+            # start_time = time.time()
+            # while process_started.count(False) > 0:
+            #     time.sleep(1)
+            #     elapsed_time = time.time() - start_time
+            #     n_running = float(max(1, process_running.count(True)))
+            #     loadavg_1min = os.getloadavg()[0]
+            #     loadavg_discount = 16.0 if elapsed_time < 60 else 0
+            #     run_more = (loadavg_1min - loadavg_discount) / n_running < n_cpu - loadavg_1min
+            #     if run_more:
+            #         results.append(pool.apply_async(optimize_pt, args=(cand2[process_index],b,comp,pend,vals,copy.copy(self))))
+            #         process_started[process_index] = True
+            #         process_running[process_index] = True
+            #         process_index += 1
+            # while process_running.count(True) > 0:
+            #     time.sleep(1)
+            #     process_running = [not p.ready() for p in results]
 
             for res in results:
                 cand = np.vstack((cand, res.get(1e8)))
