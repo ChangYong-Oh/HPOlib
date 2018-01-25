de352713182affe29377bb6126504cb3d025d206
ChangYong-Oh <changyong.oh0224@gmail.com>
2018-01-25 20:05:47 +0100

    shell command for srun command contined execution

--- benchmarks/srun_HPOlib.sh
+++ benchmarks/srun_HPOlib.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun -C cpunode  HPOlib-run -o ../optimizers/spearmint/spearmint_april2013 -s $SEED --cwd branin_20"
+eval "srun -C cpunode  HPOlib-run -o ../optimizers/spearmint/spearmint_april2013_mod -s $SEED --cwd branin_20"
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun -C cpunode  HPOlib-run -o ../optimizers/spearmint/spearmint_april2013 -s $SEED --cwd hartmann6_20"
+eval "srun -C cpunode  HPOlib-run -o ../optimizers/spearmint/spearmint_april2013_mod -s $SEED --cwd hartmann6_20"
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun -C cpunode  HPOlib-run -o ../optimizers/spearmint/spearmint_april2013 -s $SEED --cwd rosenbrock_20"
+eval "srun -C cpunode  HPOlib-run -o ../optimizers/spearmint/spearmint_april2013_mod -s $SEED --cwd rosenbrock_20"
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun -C cpunode  HPOlib-run -o ../optimizers/spearmint/spearmint_april2013 -s $SEED --cwd levy_20"
+eval "srun -C cpunode  HPOlib-run -o ../optimizers/spearmint/spearmint_april2013_mod -s $SEED --cwd levy_20"
 
 
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun -C cpunode  HPOlib-run -o ../optimizers/spearmint/spearmint_april2013 -s $SEED --cwd branin_50"
+eval "srun -C cpunode  HPOlib-run -o ../optimizers/spearmint/spearmint_april2013_mod -s $SEED --cwd branin_50"
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun -C cpunode  HPOlib-run -o ../optimizers/spearmint/spearmint_april2013 -s $SEED --cwd hartmann6_50"
+eval "srun -C cpunode  HPOlib-run -o ../optimizers/spearmint/spearmint_april2013_mod -s $SEED --cwd hartmann6_50"
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun -C cpunode  HPOlib-run -o ../optimizers/spearmint/spearmint_april2013 -s $SEED --cwd rosenbrock_50"
+eval "srun -C cpunode  HPOlib-run -o ../optimizers/spearmint/spearmint_april2013_mod -s $SEED --cwd rosenbrock_50"
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun -C cpunode  HPOlib-run -o ../optimizers/spearmint/spearmint_april2013 -s $SEED --cwd levy_50"
+eval "srun -C cpunode  HPOlib-run -o ../optimizers/spearmint/spearmint_april2013_mod -s $SEED --cwd levy_50"
 
 
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun -C cpunode  HPOlib-run -o ../optimizers/spearmint/spearmint_april2013 -s $SEED --cwd branin_100"
+eval "srun -C cpunode  HPOlib-run -o ../optimizers/spearmint/spearmint_april2013_mod -s $SEED --cwd branin_100"
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun -C cpunode  HPOlib-run -o ../optimizers/spearmint/spearmint_april2013 -s $SEED --cwd hartmann6_100"
+eval "srun -C cpunode  HPOlib-run -o ../optimizers/spearmint/spearmint_april2013_mod -s $SEED --cwd hartmann6_100"
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun -C cpunode  HPOlib-run -o ../optimizers/spearmint/spearmint_april2013 -s $SEED --cwd rosenbrock_100"
+eval "srun -C cpunode  HPOlib-run -o ../optimizers/spearmint/spearmint_april2013_mod -s $SEED --cwd rosenbrock_100"
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun -C cpunode  HPOlib-run -o ../optimizers/spearmint/spearmint_april2013 -s $SEED --cwd levy_100"
+eval "srun -C cpunode  HPOlib-run -o ../optimizers/spearmint/spearmint_april2013_mod -s $SEED --cwd levy_100"
