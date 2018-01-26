acef8bd89184012d9ef6c44682240135e3756e00
ChangYong-Oh <changyong.oh0224@gmail.com>
2018-01-26 01:10:37 +0100

    up to date

--- benchmarks/srun_SMACTPE.sh
+++ benchmarks/srun_SMACTPE.sh
@@ -1,60 +1,184 @@
 #!/bin/bash
 
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun HPOlib-run -o ../optimizers/tpe/hyperopt_august2013_mod -s $SEED --cwd branin_20"
+COMMAND="srun HPOlib-run -o ../optimizers/tpe/hyperopt_august2013_mod -s $SEED --cwd branin_20"
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+eval "$COMMAND"
+
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun HPOlib-run -o ../optimizers/tpe/hyperopt_august2013_mod -s $SEED --cwd hartmann6_20"
+COMMAND="srun HPOlib-run -o ../optimizers/tpe/hyperopt_august2013_mod -s $SEED --cwd hartmann6_20"
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+eval "$COMMAND"
+
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun HPOlib-run -o ../optimizers/tpe/hyperopt_august2013_mod -s $SEED --cwd rosenbrock_20"
+COMMAND="srun HPOlib-run -o ../optimizers/tpe/hyperopt_august2013_mod -s $SEED --cwd rosenbrock_20"
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+eval "$COMMAND"
+
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun HPOlib-run -o ../optimizers/tpe/hyperopt_august2013_mod -s $SEED --cwd levy_20"
+COMMAND="srun HPOlib-run -o ../optimizers/tpe/hyperopt_august2013_mod -s $SEED --cwd levy_20"
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+eval "$COMMAND"
+
+
 
 
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun HPOlib-run -o ../optimizers/smac/smac_2_10_00-dev -s $SEED --cwd branin_20"
+COMMAND="srun HPOlib-run -o ../optimizers/smac/smac_2_10_00-dev -s $SEED --cwd branin_20"
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+eval "$COMMAND"
+
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun HPOlib-run -o ../optimizers/smac/smac_2_10_00-dev -s $SEED --cwd hartmann6_20"
+COMMAND="srun HPOlib-run -o ../optimizers/smac/smac_2_10_00-dev -s $SEED --cwd hartmann6_20"
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+eval "$COMMAND"
+
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun HPOlib-run -o ../optimizers/smac/smac_2_10_00-dev -s $SEED --cwd rosenbrock_20"
+COMMAND="srun HPOlib-run -o ../optimizers/smac/smac_2_10_00-dev -s $SEED --cwd rosenbrock_20"
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+eval "$COMMAND"
+
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun HPOlib-run -o ../optimizers/smac/smac_2_10_00-dev -s $SEED --cwd levy_20"
+COMMAND="srun HPOlib-run -o ../optimizers/smac/smac_2_10_00-dev -s $SEED --cwd levy_20"
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+eval "$COMMAND"
+
+
 
 
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun HPOlib-run -o ../optimizers/tpe/hyperopt_august2013_mod -s $SEED --cwd branin_50"
+COMMAND="srun HPOlib-run -o ../optimizers/tpe/hyperopt_august2013_mod -s $SEED --cwd branin_50"
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+eval "$COMMAND"
+
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun HPOlib-run -o ../optimizers/tpe/hyperopt_august2013_mod -s $SEED --cwd hartmann6_50"
+COMMAND="srun HPOlib-run -o ../optimizers/tpe/hyperopt_august2013_mod -s $SEED --cwd hartmann6_50"
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+eval "$COMMAND"
+
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun HPOlib-run -o ../optimizers/tpe/hyperopt_august2013_mod -s $SEED --cwd rosenbrock_50"
+COMMAND="srun HPOlib-run -o ../optimizers/tpe/hyperopt_august2013_mod -s $SEED --cwd rosenbrock_50"
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+eval "$COMMAND"
+
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun HPOlib-run -o ../optimizers/tpe/hyperopt_august2013_mod -s $SEED --cwd levy_50"
+COMMAND="srun HPOlib-run -o ../optimizers/tpe/hyperopt_august2013_mod -s $SEED --cwd levy_50"
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+eval "$COMMAND"
+
+
 
 
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun HPOlib-run -o ../optimizers/smac/smac_2_10_00-dev -s $SEED --cwd branin_50"
+COMMAND="srun HPOlib-run -o ../optimizers/smac/smac_2_10_00-dev -s $SEED --cwd branin_50"
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+eval "$COMMAND"
+
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun HPOlib-run -o ../optimizers/smac/smac_2_10_00-dev -s $SEED --cwd hartmann6_50"
+COMMAND="srun HPOlib-run -o ../optimizers/smac/smac_2_10_00-dev -s $SEED --cwd hartmann6_50"
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+eval "$COMMAND"
+
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun HPOlib-run -o ../optimizers/smac/smac_2_10_00-dev -s $SEED --cwd rosenbrock_50"
+COMMAND="srun HPOlib-run -o ../optimizers/smac/smac_2_10_00-dev -s $SEED --cwd rosenbrock_50"
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+eval "$COMMAND"
+
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun HPOlib-run -o ../optimizers/smac/smac_2_10_00-dev -s $SEED --cwd levy_50"
+COMMAND="srun HPOlib-run -o ../optimizers/smac/smac_2_10_00-dev -s $SEED --cwd levy_50"
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+eval "$COMMAND"
+
+
 
 
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun HPOlib-run -o ../optimizers/tpe/hyperopt_august2013_mod -s $SEED --cwd branin_100"
+COMMAND="srun HPOlib-run -o ../optimizers/tpe/hyperopt_august2013_mod -s $SEED --cwd branin_100"
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+eval "$COMMAND"
+
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun HPOlib-run -o ../optimizers/tpe/hyperopt_august2013_mod -s $SEED --cwd hartmann6_100"
+COMMAND="srun HPOlib-run -o ../optimizers/tpe/hyperopt_august2013_mod -s $SEED --cwd hartmann6_100"
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+eval "$COMMAND"
+
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun HPOlib-run -o ../optimizers/tpe/hyperopt_august2013_mod -s $SEED --cwd rosenbrock_100"
+COMMAND="srun HPOlib-run -o ../optimizers/tpe/hyperopt_august2013_mod -s $SEED --cwd rosenbrock_100"
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+eval "$COMMAND"
+
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun HPOlib-run -o ../optimizers/tpe/hyperopt_august2013_mod -s $SEED --cwd levy_100"
+COMMAND="srun HPOlib-run -o ../optimizers/tpe/hyperopt_august2013_mod -s $SEED --cwd levy_100"
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+eval "$COMMAND"
+
+
 
 
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun HPOlib-run -o ../optimizers/smac/smac_2_10_00-dev -s $SEED --cwd branin_100"
+COMMAND="srun HPOlib-run -o ../optimizers/smac/smac_2_10_00-dev -s $SEED --cwd branin_100"
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+eval "$COMMAND"
+
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun HPOlib-run -o ../optimizers/smac/smac_2_10_00-dev -s $SEED --cwd hartmann6_100"
+COMMAND="srun HPOlib-run -o ../optimizers/smac/smac_2_10_00-dev -s $SEED --cwd hartmann6_100"
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+eval "$COMMAND"
+
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun HPOlib-run -o ../optimizers/smac/smac_2_10_00-dev -s $SEED --cwd rosenbrock_100"
+COMMAND="srun HPOlib-run -o ../optimizers/smac/smac_2_10_00-dev -s $SEED --cwd rosenbrock_100"
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+eval "$COMMAND"
+
 SEED=$(( ( RANDOM % 20000 )  + 1 ))
-eval "srun HPOlib-run -o ../optimizers/smac/smac_2_10_00-dev -s $SEED --cwd levy_100"
+COMMAND="srun HPOlib-run -o ../optimizers/smac/smac_2_10_00-dev -s $SEED --cwd levy_100"
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+echo "==============================$srun COMMAND=============================="
+eval "$COMMAND"
